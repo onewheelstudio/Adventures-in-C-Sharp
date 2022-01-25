942e35667a8fb30eba56fd302c0359101f773623
onewheelstudio <jeremytylerwolf@gmail.com>
2022-01-24 20:50:58 -0700

    Added Object Pool 2.0

---
+++ Object Pool 2/ObjectPool.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T> : IPool<T> where T : Component, IPoolable<T>
+{
+    public ObjectPool(GameObject pooledObject, int numToSpawn = 0)
+    {
+        this.prefab = pooledObject;
+        Spawn(numToSpawn);
+    }
+
+    public ObjectPool(GameObject pooledObject, System.Action<T> pullObject, System.Action<T> pushObject)
+    {
+        this.prefab = pooledObject;
+        this.pullObject = pullObject;
+        this.pushObject = pushObject;
+    }
+
+    private System.Action<T> pullObject;
+    private System.Action<T> pushObject;
+    private Stack<T> pooledObjects = new Stack<T>();
+    private GameObject prefab;
+    public int pooledCount
+    {
+        get
+        {
+            return pooledObjects.Count;
+        }
+    }
+
+    public T Pull()
+    {
+        T t;
+        if (pooledCount > 0)
+            t = pooledObjects.Pop();
+        else
+            t = GameObject.Instantiate(prefab).GetComponent<T>();
+
+        t.gameObject.SetActive(true); //ensure the object is on
+        t.Initialize(Push);
+
+        //allow default behavior and turning object back on
+        if (pullObject != null)
+            pullObject.Invoke(t);
+
+        return t;
+    }
+
+    public T Pull(Vector3 position)
+    {
+        T t = Pull();
+        t.transform.position = position;
+        return t;
+    }
+
+    public T Pull(Vector3 position, Quaternion rotation)
+    {
+        T t = Pull();
+        t.transform.position = position;
+        t.transform.rotation = rotation;
+        return t;
+    }
+
+    public GameObject PullGameObject()
+    {
+        return Pull().gameObject;
+    }
+
+    public GameObject PullGameObject(Vector3 position)
+    {
+        GameObject go = Pull().gameObject;
+        go.transform.position = position;
+        return go;
+    }
+
+    public GameObject PullGameObject(Vector3 position, Quaternion rotation)
+    {
+        GameObject go = Pull().gameObject;
+        go.transform.position = position;
+        go.transform.rotation = rotation;
+        return go;
+    }
+
+    public void Push(T t)
+    {
+        pooledObjects.Push(t);
+
+        //create default behavior to turn off objects
+        if (pushObject != null)
+            pushObject.Invoke(t);
+
+        t.gameObject.SetActive(false);
+    }
+
+    private void Spawn(int number)
+    {
+        T t;
+
+        for (int i = 0; i < number; i++)
+        {
+            t = GameObject.Instantiate(prefab).GetComponent<T>();
+            pooledObjects.Push(t);
+            t.gameObject.SetActive(false);
+        }
+    }
+}
+
+public interface IPool<T>
+{
+    T Pull();
+    void Push(T gameObject);
+}
+
+public interface IPoolable<T>
+{
+    void Initialize(System.Action<T> returnAction);
+    void ReturnToPool();
+}
